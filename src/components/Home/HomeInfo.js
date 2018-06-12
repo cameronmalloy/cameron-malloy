eba8a0991b77600b16b03b89efc10df42aa8dfbb
Cameron Malloy <cmalloy@berkeley.edu>
2018-06-11 22:32:53 -0700

    Added to updates

--- src/components/Home/HomeInfo.js
+++ src/components/Home/HomeInfo.js
@@ -6,8 +6,9 @@ const HomeInfo = () => (
         <div className="two-rem-text">
             <div className="info-title-title">Updates</div>
             <div className="seventy-five-percent-container">
-                <HomeUpdate title="Updated About Page" highlights="Works better with smaller webpage sizes"date="6/10/18" link="/about"/>
-                <HomeUpdate title="Added Github Link" highlights="Restaurant Project" date="6/10/18" link="projects1"/>
+                <HomeUpdate title="Updated Dynamacy" highlights="Home Page, Header" date="6/11/18" link="/" />
+                <HomeUpdate title="Updated About Page" highlights="Works better with smaller webpage sizes" date="6/10/18" link="/about" />
+                <HomeUpdate title="Added Github Link" highlights="Restaurant Project" date="6/10/18" link="projects1" />
             </div>
         </div>
     </div>
