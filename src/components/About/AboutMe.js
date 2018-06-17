8aa0e8c91e41a47c2f0594e1340ed32a93cb2786
Cameron Malloy <cmalloy@berkeley.edu>
2018-06-17 00:05:44 -0700

    Fixed typo

--- src/components/About/AboutMe.js
+++ src/components/About/AboutMe.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutMe = () => (
     <div className="container-about-me">
         <p>Hello, I’m Cameron Malloy. I’m a data nerd and coding geek. I like all things that have large amounts in data, because there’s always something to find. Every dataset has a secret and as a data scientist, it’s my job to find it!</p>
-        <p>I started coding at the considerably old age (in this era) of 18. I started from having no idea what a pointer was to solving machine learning problems bay area universities and companies.</p>
+        <p>I started coding at the considerably old age (in this era) of 18. I started from having no idea what a pointer was to solving machine learning problems at Bay Area universities and companies.</p>
         <p>I love sports, especially baseball. I hate coffee even though I worked at Starbucks as a barista for a year. I love avocado toast, but I never buy them, they are way too overpriced.</p>
     </div>
 )
