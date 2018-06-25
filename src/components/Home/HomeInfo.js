3f2c2fe5450fa272a2b1edda2e92d62670ba69c3
Cameron Malloy <cmalloy@berkeley.edu>
2018-06-24 22:42:21 -0700

    Added week 2 to home update

--- src/components/Home/HomeInfo.js
+++ src/components/Home/HomeInfo.js
@@ -6,7 +6,8 @@ const HomeInfo = () => (
         <div className="two-rem-text">
             <div className="info-title-title">Updates</div>
             <div className="seventy-five-percent-container">
-                <HomeUpdate title="Added Week 1 (61A)" highlights="Summary, Links" date="6/23/18" link="/teaching/02" />
+                <HomeUpdate title="Added Week 2 (61A)" highlights="Functions" date="6/24/18" link="/teaching/03" />
+                <HomeUpdate title="Added Week 1 (61A)" highlights="Introduction" date="6/23/18" link="/teaching/02" />
                 <HomeUpdate title="Updated Dynamacy" highlights="Home Page, Header" date="6/11/18" link="/" />
                 <HomeUpdate title="Updated About Page" highlights="Works better with smaller webpage sizes" date="6/10/18" link="/about" />
                 <HomeUpdate title="Added Github Link" highlights="Restaurant Project" date="6/10/18" link="projects1" />
